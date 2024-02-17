42fe4df517c5916b77c26c8f79030ab8af1fe46d
tfkhdyt <tfkhdyt@proton.me>
2024-02-17 12:11:27 +0700

    fix: Redirect user to saved location if available on index route

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,15 @@
-import type { MetaFunction } from "@remix-run/node";
+import { LoaderFunctionArgs, redirect } from '@remix-run/node';
+import { getSession } from '~/sessions';
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const session = await getSession(request.headers.get('Cookie'));
+  if (session.has('locationId')) {
+    return redirect(`/${session.get('locationId')}`);
+  }
+
+  return null;
 };
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
