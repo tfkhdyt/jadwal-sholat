4f3310c34dff2d126fe2938687c7017a14e9a681
tfkhdyt <tfkhdyt@proton.me>
2024-06-15 15:45:38 +0700

    style: format

--- app/hooks/use-closest-adzan.ts
+++ app/hooks/use-closest-adzan.ts
@@ -3,15 +3,15 @@ import { getClosestAdzan, timeRemaining } from '~/lib/utils';
 import { Jadwal } from '~/types/adzan';
 
 export function useClosestAdzan(jadwal: Jadwal[]) {
-  const [closestUpcomingAdzan, setClosestUpcomingAdzan] =
-    useState<Jadwal | null>(getClosestAdzan(jadwal));
-  const [timeRemainingToClosestAdzan, setTimeRemainingToClosestAdzan] =
-    useState(timeRemaining(closestUpcomingAdzan?.time ?? null));
+  const [closestUpcomingAdzan, setClosestUpcomingAdzan] = useState<Jadwal | null>(getClosestAdzan(jadwal));
+  const [timeRemainingToClosestAdzan, setTimeRemainingToClosestAdzan] = useState(
+    timeRemaining(closestUpcomingAdzan?.time ?? null)
+  );
 
   useEffect(() => {
     const interval = setInterval(() => {
       const closestAdzan = getClosestAdzan(jadwal);
-      const timeToAdzan = timeRemaining(closestUpcomingAdzan?.time ?? null);
+      const timeToAdzan = timeRemaining(closestUpcomingAdzan?.time ?? null, jadwal[0].time);
 
       setTimeRemainingToClosestAdzan(timeToAdzan);
       if (closestAdzan) {
